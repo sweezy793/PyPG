b8fdc4a2d81d028cd84f99d2e22b93a1337dc52b
sweezy793 <sarthaks793@gmail.com>
2018-09-13 01:22:33 +0530

    Player class created

--- venv/game.py
+++ venv/game.py
@@ -1,3 +1,5 @@
+import random
+
 class colors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -8,3 +10,19 @@ class colors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+
+class Player:
+    def __init__(self,health,mana,attack,defence,magic):
+        self.maxhealth = health
+        self.health = health
+        self.maxmana = mana
+        self.mana = mana
+        self.maxattack = attack+10
+        self.minattack = attack-10
+        self.defence = defence
+        self.magic = magic
+        self.moves = ["Attack", "Spell"]
+
+    def damage(self):
+        return random.randrange(self.maxattack,self.minattack)
+    
